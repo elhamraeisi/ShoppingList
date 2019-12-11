0a2170bd09c62d83bc2f972a8747c2e3a1bea8e4
elham zaribaf <elhamzaribaf@elhams-macbook-pro.home>
2019-12-11 23:11:27 +0100

    configuration et le socle de la partie front

---
+++ front/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            listProduits: []
+        };
+    }
+
+    componentDidMount() {
+        
+    }
+
+    render() {
+
+        
+        return (
+            <div >
+                Shopping List App
+            </div>
+        )
+    }
+}
+
+export default App
---
+++ front/src/services/produitService.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import * as Constants from '../Constants'
+export default {
+  
+  /*
+  Renvoie tous les produits existants en base de données
+  */
+  recupererTousProduits: async () => {
+    let res = await axios.get(Constants.API_ENDPOINT + 'produit');
+    return res.data;
+  },
+  /*
+  Enregistrer un produit
+  */
+  enregistrerProduit: async (produit) => {
+    let res = await axios.post(Constants.API_ENDPOINT +'produit',{
+      nom: produit.nom,
+      description: produit.description,
+      prix: produit.prix,
+      quantite: produit.quantite
+    });
+    return res.data;
+  },
+  /*
+  Modifier un produit
+  */
+  modifierProduit: async (produit) => {
+    let res = await axios.put(Constants.API_ENDPOINT +'produit/' + produit.id,{
+      nom: produit.nom,
+      description: produit.description,
+      prix: produit.prix,
+      quantite: produit.quantite
+    });
+    return res.data;
+  },
+  /*
+  Supprimer un produit par Id
+  */
+  supprimerProduitParId: async (id) => {
+    let res = await axios.delete(Constants.API_ENDPOINT +'produit/' + id);
+    return res.data;
+  }
+
+}
