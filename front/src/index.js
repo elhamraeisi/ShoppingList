70ab292886ee6a7ec5b727b56eca9d778035c89a
elham zaribaf <elhamzaribaf@elhams-macbook-pro.home>
2019-12-14 23:06:49 +0100

    pages de connexion et inscription

--- front/src/index.js
+++ front/src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import App from './App';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ConnexionPage from './pages/ConnexionPage';
+import ProduitsPage from './pages/ProtuitsPage';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+class App extends Component {
+    constructor() {
+      super();
+      this.state = {
+        name: 'React'
+      };
+    }
+  
+    render() {
+      return (
+        <Router>
+          <div>
+            <Route exact path="/" component={ConnexionPage} />
+            <Route path="/produits" component={ProduitsPage} />
+          </div>
+        </Router>
+      );
+    }
+  }
+
+  render(<App />, document.getElementById('root'));
+
 serviceWorker.unregister();
--- front/src/services/produitService.js
+++ front/src/services/produitService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import * as Constants from '../Constants'
+import * as Constants from '../commun/Constants'
 export default {
   
   /*
